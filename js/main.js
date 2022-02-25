ed57b85255000e48da61885802a264a7e08b552e
Barth0l0mew <barth0l0mew@mail.ru>
2022-02-25 13:34:52 +0300

    menu

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let menuBtn = document.querySelector(".menuBottom")
+let menuList = document.querySelector('.navbar')
+let search = document.querySelector('.header-wraper__search')
+
+menuBtn.addEventListener('click',menu);
+function menu(){
+   menuList.classList.toggle('navbar--visible')
+    
+    console.log("asdasd")
+}

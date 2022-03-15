a7c5433e8c9ab00cb86578e9261dd9ebf0cf052c
Barth0l0mew <barth0l0mew@mail.ru>
2022-03-15 15:45:21 +0300

    modal

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 let menuBtn = document.querySelector(".menuBottom")
 let menuList = document.querySelector('.navbar')
 let search = document.querySelector('.header-wraper__search')
+let modalClose = document.querySelector('.modal__close')
+let modal = document.querySelector('.modal')
+let viewBtn = document.querySelector('.booking-button')
 
 menuBtn.addEventListener('click',menu);
 function menu(){
@@ -8,3 +11,9 @@ function menu(){
     
     console.log("asdasd")
 }
+modalClose.addEventListener('click',modalHiden);
+function modalHiden(){
+    console.log('hihi');
+    modal.classList.toggle('modal--visible')
+}
+viewBtn.addEventListener('click',modalHiden )
